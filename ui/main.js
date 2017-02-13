7a53b4debe6a2ce46d7ca177e3c8651d09d52e2e
bharathreddy911 <rbkroczz@gmail.com>
2017-02-13 09:04:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,9 @@
 var button=document.getElementById('counter');
+var counter=0;
 
 button.onClick=function(){
-    
-    var request=XMLHttpRequest();
-    
-    request.onreadystatechange=function(){
-        if(request.readychange===XMLHttprequest.DONE){
-            if(request.status===200){
-                var counter=request.responsetext;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
-            }
-        }
-    };
-    request.open('GET','http://bharathreddy911.imad.hasura-app.io/counter',true);
-    request.send(null);
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
+
