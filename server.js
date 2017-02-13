d287c2e448a32fea3a3dbf518ca80369a070a1d5
bharathreddy911 <rbkroczz@gmail.com>
2017-02-13 07:48:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,6 +51,11 @@ function createtemplate(data){
 app.get('/article-one',function(req,res){
    res.send(createtemplate(articleone));
 });
+counter=0;
+app.get('/counter',function(req,res){
+   counter=counter+1;
+   res.send(counter.toString());
+});
 
 app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
